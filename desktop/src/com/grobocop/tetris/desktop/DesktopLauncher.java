96e6f802e0bd2dda535056705c74c365cbdaf8fb
grobocop12 <chasnik97876@gmail.com>
2022-10-15 09:24:28 +0200

    api changed to lwjgl3

--- desktop/src/com/grobocop/tetris/desktop/DesktopLauncher.java
+++ desktop/src/com/grobocop/tetris/desktop/DesktopLauncher.java
@@ -1,7 +1,7 @@
 package com.grobocop.tetris.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.grobocop.tetris.TetrisGame;
 
 import static com.grobocop.tetris.GameConstants.HEIGHT;
@@ -10,10 +10,8 @@ import static com.grobocop.tetris.GameConstants.WIDTH;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.height = (int) HEIGHT;
-        config.width = (int) WIDTH;
-        config.forceExit = false;
-        new LwjglApplication(new TetrisGame(), config);
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowedMode((int) WIDTH, (int) HEIGHT);
+        new Lwjgl3Application(new TetrisGame(), config);
     }
 }
